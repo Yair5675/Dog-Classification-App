bc504ad51ed998cc11d19b0fb0a94d6ea6e3bb16
Yair5675 <yairziv5675@gmail.com>
2023-06-14 17:50:59 +0300

    Added the "getInfo" function signiture

--- app/src/main/java/com/example/dogclassificationapp/classifier_logic/WikiAPI.java
+++ app/src/main/java/com/example/dogclassificationapp/classifier_logic/WikiAPI.java
@@ -1,7 +1,20 @@
 package com.example.dogclassificationapp.classifier_logic;
 
+import java.util.Optional;
+
 /**
  * A utility class for retrieving information from the Wikipedia API.
  */
 public class WikiAPI {
+
+    /**
+     * The main function of the class, returns information from Wikipedia about the given breed.
+     * @param breed The name of the dog breed that will be searched.
+     * @return If the API call was successful the information is returned, but if an error occurred
+     *         an empty optional is returned.
+     */
+    public static Optional<String> getInfo(String breed) {
+        // TODO: Complete the getInfo function based on the documentation
+        return Optional.empty();
+    }
 }

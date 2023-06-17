8595cf29e99a1bd93b3d991420a9c91e83a75aad
Yair5675 <yairziv5675@gmail.com>
2023-06-17 13:27:15 +0300

    Added endpoint URL for loading images

--- app/src/main/java/com/example/dogclassificationapp/api_handlers/DogImagesAPI.java
+++ app/src/main/java/com/example/dogclassificationapp/api_handlers/DogImagesAPI.java
@@ -4,4 +4,8 @@ package com.example.dogclassificationapp.api_handlers;
  * A utility class to retrieve images of specific dog breeds from the dog API.
  */
 public class DogImagesAPI {
+
+    // The endpoint to receive images of dogs by breeds and sub-breeds. The arguments that should
+    // be replaced are: "{breed}", "{sub_breed}", "{num_images}":
+    private static final String DOG_IMAGES_ENDPOINT = "https://dog.ceo/api/breed/{breed}/{sub_breed}/images/random/{num_images}";
 }

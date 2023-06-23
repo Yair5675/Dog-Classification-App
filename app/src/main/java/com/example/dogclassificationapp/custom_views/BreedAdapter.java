9e730f5f00be5dda99c142d0507ea4924c36c02e
Yair5675 <yairziv5675@gmail.com>
2023-06-23 23:28:12 +0300

    Added the "breed_row.xml" components as attributes to the "BreedVH" class

--- app/src/main/java/com/example/dogclassificationapp/custom_views/BreedAdapter.java
+++ app/src/main/java/com/example/dogclassificationapp/custom_views/BreedAdapter.java
@@ -2,10 +2,14 @@ package com.example.dogclassificationapp.custom_views;
 
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dogclassificationapp.R;
 import com.example.dogclassificationapp.classifier_logic.Breed;
 
 import java.util.ArrayList;
@@ -39,8 +43,39 @@ public class BreedAdapter extends RecyclerView.Adapter<BreedAdapter.BreedVH> {
     }
 
     public static class BreedVH extends RecyclerView.ViewHolder {
+
+        // The actual information paragraph (with a bonus image at the end):
+        private final TextView infoTv;
+
+        // The textView that will present the dog's breed:
+        private final TextView breedTV;
+
+        // The title of the information paragraph:
+        private final TextView infoTitleTV;
+
+        // The textView that will present the confidence of the model in the current breed:
+        private final TextView confidenceTV;
+
+        // The image that will be shown without expanding the breed:
+        private final ImageView shownBreedImgV;
+
+        // The expandable part of the row:
+        private final ConstraintLayout expandableLayout;
+
         public BreedVH(@NonNull View itemView) {
             super(itemView);
+
+            // Loading the main image:
+            this.shownBreedImgV = itemView.findViewById(R.id.shown_breed_img);
+
+            // Loading all the text views:
+            this.breedTV = itemView.findViewById(R.id.breed_name_tv);
+            this.confidenceTV = itemView.findViewById(R.id.breed_confidence_tv);
+            this.infoTitleTV = itemView.findViewById(R.id.breed_info_title);
+            this.infoTv = itemView.findViewById(R.id.breed_info_text);
+
+            // Loading the expandable layout:
+            this.expandableLayout = itemView.findViewById(R.id.breed_expandable_layout);
         }
     }
 }

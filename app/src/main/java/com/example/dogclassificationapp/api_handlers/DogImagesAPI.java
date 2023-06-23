4eb51053d1cd0c23006048ddf2014725144a1ef4
Yair5675 <yairziv5675@gmail.com>
2023-06-23 14:28:55 +0300

    Created "getImagesUrls" function signature

--- app/src/main/java/com/example/dogclassificationapp/api_handlers/DogImagesAPI.java
+++ app/src/main/java/com/example/dogclassificationapp/api_handlers/DogImagesAPI.java
@@ -1,5 +1,7 @@
 package com.example.dogclassificationapp.api_handlers;
 
+import java.util.ArrayList;
+
 /**
  * A utility class to retrieve images of specific dog breeds from the dog API.
  */
@@ -8,4 +10,15 @@ public class DogImagesAPI {
     // The endpoint to receive images of dogs by breeds and sub-breeds. The arguments that should
     // be replaced are: "{breed}", "{sub_breed}", "{num_images}":
     private static final String DOG_IMAGES_ENDPOINT = "https://dog.ceo/api/breed/{breed}/{sub_breed}/images/random/{num_images}";
+
+    /**
+     * Returns a list of image URLs from the dog API. The length of the list is the amount of images
+     * that was given as a parameter.
+     * @param numImages The amount of images that will be returned
+     * @return A list of image URLs from the dog API.
+     */
+    public static ArrayList<String> getImagesURLs(int numImages) {
+        // TODO: Complete function per documentation.
+        return null;
+    }
 }

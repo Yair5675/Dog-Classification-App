a2b17366aa8c2c60dfb099d86007c085c1222743
Yair5675 <yairziv5675@gmail.com>
2023-06-23 22:52:43 +0300

    Made BreedAdapter extend RecyclerView.Adapter and created an inner class to serve as a custom ViewHolder

--- app/src/main/java/com/example/dogclassificationapp/custom_views/BreedAdapter.java
+++ app/src/main/java/com/example/dogclassificationapp/custom_views/BreedAdapter.java
@@ -1,7 +1,34 @@
 package com.example.dogclassificationapp.custom_views;
 
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
 /**
  * This class's purpose is to bind Breed objects into a recyclerView
  */
-public class BreedAdapter {
+public class BreedAdapter extends RecyclerView.Adapter<BreedAdapter.BreedVH> {
+    @NonNull
+    @Override
+    public BreedVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BreedVH holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public static class BreedVH extends RecyclerView.ViewHolder {
+        public BreedVH(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }

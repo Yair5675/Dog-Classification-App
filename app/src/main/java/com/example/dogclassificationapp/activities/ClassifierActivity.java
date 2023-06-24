818e883c6f56902973b0b93e92bd16feac6bbd90
Yair5675 <yairziv5675@gmail.com>
2023-06-24 16:19:45 +0300

    Set the bitmap that was given from the previous activity as the main image

--- app/src/main/java/com/example/dogclassificationapp/activities/ClassifierActivity.java
+++ app/src/main/java/com/example/dogclassificationapp/activities/ClassifierActivity.java
@@ -2,7 +2,10 @@ package com.example.dogclassificationapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.example.dogclassificationapp.R;
 
@@ -12,5 +15,13 @@ public class ClassifierActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_classifier);
+
+        // Loading the intent from the previous activity:
+        final Intent intent = getIntent();
+        final Bitmap chosenImg = intent.getParcelableExtra("dog_image");
+
+        // Setting the main image as the bitmap:
+        final ImageView mainImg = findViewById(R.id.main_img_classifier);
+        mainImg.setImageBitmap(chosenImg);
     }
 }

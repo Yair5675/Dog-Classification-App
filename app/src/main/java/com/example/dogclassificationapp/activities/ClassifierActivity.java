82dd1c44544982c5a8917347fbdf7912ad0af425
Yair5675 <yairziv5675@gmail.com>
2023-06-24 16:28:34 +0300

    Added attributes "classifier" and "breedsListView" and loaded them in the onCreate method

--- app/src/main/java/com/example/dogclassificationapp/activities/ClassifierActivity.java
+++ app/src/main/java/com/example/dogclassificationapp/activities/ClassifierActivity.java
@@ -1,6 +1,7 @@
 package com.example.dogclassificationapp.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -8,8 +9,21 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 import com.example.dogclassificationapp.R;
+import com.example.dogclassificationapp.classifier_logic.Breed;
+import com.example.dogclassificationapp.classifier_logic.DogClassifier;
+
+import java.util.ArrayList;
 
 public class ClassifierActivity extends AppCompatActivity {
+
+    // A list of all breeds that will be displayed in the recycler view
+    private ArrayList<Breed> breeds;
+
+    // The TF-Lite model that will classify the given image:
+    private DogClassifier classifier;
+
+    // The recycler view that will present all the breeds:
+    private RecyclerView breedsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +37,12 @@ public class ClassifierActivity extends AppCompatActivity {
         // Setting the main image as the bitmap:
         final ImageView mainImg = findViewById(R.id.main_img_classifier);
         mainImg.setImageBitmap(chosenImg);
+
+        // Creating the classifier:
+        classifier = new DogClassifier(this, getAssets());
+
+        // Loading the recycler-view:
+        breedsListView = findViewById(R.id.breeds_list_classifier);
     }
 
     /**

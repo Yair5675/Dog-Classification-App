7fcd0cc9a005cf1d8fcac58fc66a4790d86b28d7
Yair5675 <yairziv5675@gmail.com>
2023-06-24 16:40:50 +0300

    Completed the "initData" function

--- app/src/main/java/com/example/dogclassificationapp/activities/ClassifierActivity.java
+++ app/src/main/java/com/example/dogclassificationapp/activities/ClassifierActivity.java
@@ -7,12 +7,16 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dogclassificationapp.R;
 import com.example.dogclassificationapp.classifier_logic.Breed;
 import com.example.dogclassificationapp.classifier_logic.DogClassifier;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class ClassifierActivity extends AppCompatActivity {
 
@@ -43,14 +47,30 @@ public class ClassifierActivity extends AppCompatActivity {
 
         // Loading the recycler-view:
         breedsListView = findViewById(R.id.breeds_list_classifier);
+
+        // Initializing the data:
+        this.initData(chosenImg);
     }
 
     /**
-     * Uses the TF-Lite model to classify the given image and load the breeds attribute.
+     * Uses the classifier to classify the given image and load the "breeds" attribute. The function
+     * also sets the title of the classifier activity as the breed with the highest confidence.
      * @param chosenImg The image of a dog that the user has chosen, will be classified using the
-     *                  "model" attribute
+     *                  "classifier" attribute
      */
     private void initData(Bitmap chosenImg) {
-        // TODO: Complete the function as per documentation
+        // Create the list of breeds using the classifier:
+        this.breeds = this.classifier.getModelPredictions(chosenImg, getResources()).orElse(new ArrayList<>());
+
+        // Sorting the breeds from highest confidence to lowest:
+        this.breeds.sort((b1, b2) -> Double.compare(b2.getConfidence(), b1.getConfidence()));
+
+        // Setting the title as the name of the breed with the highest confidence:
+        if (this.breeds.size() > 0) {
+            final TextView title = findViewById(R.id.result_title_classifier);
+            final String titleTxt = "Result: " + this.breeds.get(0).getFullName();
+            title.setText(titleTxt);
+        }
+
     }
 }

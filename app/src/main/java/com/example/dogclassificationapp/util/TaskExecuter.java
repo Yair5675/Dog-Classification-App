90c3a6b9b9ba65c902d00aa640446466b1e88739
Yair5675 <yairziv5675@gmail.com>
2023-06-30 19:02:49 +0300

    Created the class + documentation

---
+++ app/src/main/java/com/example/dogclassificationapp/util/TaskExecuter.java
@@ -0,0 +1,9 @@
+package com.example.dogclassificationapp.util;
+
+/**
+ * A utility class used for running asynchronous tasks. The class will repeatedly perform the task
+ * until it is successful. If the task failed, a specified amount of time will be waited before
+ * rerunning the task. The given task must return a Result variable.
+ */
+public class TaskExecuter {
+}
